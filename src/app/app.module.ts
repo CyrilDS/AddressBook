97fc96a4c45e54600019069eed21bcd702ea4fc3
dik david <dikdavid@socgen.com>
2019-04-03 17:16:22 +0200

    created home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
