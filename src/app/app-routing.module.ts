d6d81d7550556ec821995aff5f0506a6f57bb55e
Richard Phonthibsvads <richard.p.itschool@gmail.com>
2019-04-04 11:36:24 +0200

    add admin path & route to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookComponent } from './book/book.component';
+// import { AddressComponent } from './address/address.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: BookComponent },
+  //{ path: 'address', component: AddressComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
