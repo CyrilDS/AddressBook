3a3f8a290f00e7d5795ec897a00d35878a93d875
Patricia Leclerc <patricia.leclerc@live.fr>
2019-04-04 11:49:03 +0200

    address component and form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { BookComponent } from './book/book.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule, MatIconModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material/dialog';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AddressComponent } from './address/address.component';
 
 
 
@@ -19,7 +25,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   declarations: [
     AppComponent,
     BookComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +36,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     HttpClientModule,
     MatListModule,
     MatMenuModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatIconModule,
+    MatDialogModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
