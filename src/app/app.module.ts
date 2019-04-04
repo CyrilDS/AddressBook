92427c92c7d4ce12dc4fc2485b830dbffad77f9c
dik david <dikdavid@socgen.com>
2019-04-04 10:29:13 +0200

    debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,26 +9,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
-import { HomeComponent } from './home/home.component';
-=======
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-
->>>>>>> 921a50c1cd8b4057fcd4b316edf149bde8dce8ea
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    BookComponent,
-<<<<<<< HEAD
-    HomeComponent
-=======
-    NavbarComponent
->>>>>>> 921a50c1cd8b4057fcd4b316edf149bde8dce8ea
-  ],
+  declarations: [AppComponent, BookComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -42,4 +28,4 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
